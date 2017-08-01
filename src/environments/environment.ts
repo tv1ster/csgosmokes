efa30dc27f12bf707f8eb9162bc4636501012d43
Vadym <vadym.ruchka@gmail.com>
2017-08-01 19:07:24 +0300

    Deleting api keys and moving to another api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAmenYoc1z0ga-k5AZvo33shgycmLGUw40',
-    authDomain: 'csgosmokes-37d51.firebaseapp.com',
-    databaseURL: 'https://csgosmokes-37d51.firebaseio.com',
-    projectId: 'csgosmokes-37d51',
-    storageBucket: 'csgosmokes-37d51.appspot.com',
-    messagingSenderId: '656062207011'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
