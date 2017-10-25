cf365f3627c01695697f56351a5c4c7ba03d69c6
Vadym <vadym.ruchka@gmail.com>
2017-10-25 08:37:44 +0300

    Commiting api keys for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyBVJwEbJiy5vjDwknC_MxrvilaR22-7BP8',
+    authDomain: 'cs-go-smokes-1a357.firebaseapp.com',
+    databaseURL: 'https://cs-go-smokes-1a357.firebaseio.com',
+    projectId: 'cs-go-smokes-1a357',
+    storageBucket: 'cs-go-smokes-1a357.appspot.com',
+    messagingSenderId: '496228968088'
   }
 };
